72754d20ad8a134eb840fc9aa4445fe6e698f71f
nguyendinhhung04 <dinhhung5a6@gmail.com>
2025-03-20 17:57:19 +0700

    add controller to receive data from react frontend
    note: have to configure received file frim react is JSON file
    @PostMapping(value = "/create", consumes = "application/json", produces = "application/json" )

--- src/main/java/com/ptit/ttcs/bookstore/controller/HomeController.java
+++ src/main/java/com/ptit/ttcs/bookstore/controller/HomeController.java
@@ -3,11 +3,10 @@ package com.ptit.ttcs.bookstore.controller;
 import com.ptit.ttcs.bookstore.domain.User;
 import com.ptit.ttcs.bookstore.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // Cho phép React gọi API
 public class HomeController {
 
     private final UserService userService;
@@ -16,20 +15,22 @@ public class HomeController {
         this.userService = userService;
     }
 
-    @RequestMapping("/")
-    public String getHomePage(){
-        return "index.html";
+//    @RequestMapping("/")
+//    public String getHomePage(){
+//        return "index.html";
+//    }
+//
+//    @RequestMapping("/user/create")
+//    public void getCreateUserPage(){
+//        User user = new User();
+//        user.setFullname("Hung");
+//        System.out.println(userService.saveUser(user));
+//    }
+
+    @PostMapping(value = "/create", consumes = "application/json", produces = "application/json" )
+    public void createUser(@RequestBody User user) {
+        System.out.println("Received User: " + user );
     }
-
-
-    @CrossOrigin(origins = "http://localhost:3000") // Cho phép React gọi API
-    @RequestMapping("/user/create")
-    public void getCreateUserPage(){
-        User user = new User();
-        user.setFullname("Hung");
-        System.out.println(userService.saveUser(user));
-    }
-
 }
 
 
